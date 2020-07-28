f5f425f3cd47cdf598188171cc6334a091abb3bc
Lucca Hora <luccahora@gmail.com>
2020-07-28 03:51:39 -0300

    Front e rotas

---
+++ server.js
@@ -0,0 +1,26 @@
+/* import */
+const express = require('express');
+const nunjuncks = require('nunjucks');
+
+const server = express();
+
+server.use(express.static('public'));
+
+server.set("view engine", "html");
+
+nunjuncks.configure("views", {
+    express:server
+});
+
+/* Rotas / */
+server.get("/", function (req, res) {
+    return res.render("index");
+});
+
+server.get("/cursos", function (req, res) {
+    return res.render("cursos");
+});
+
+server.listen(5000, function () {
+    console.log("servidor rodando");
+});
